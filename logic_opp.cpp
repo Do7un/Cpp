d7819c09ecb8a803577884f3d3d7eb218c55b45e
Do7un <olusomiakinbo@gmail.com>
2023-07-08 11:50:49 +0100

    restart

--- logic_opp.cpp
+++ logic_opp.cpp
@@ -3,12 +3,18 @@
 int main(){
     using namespace std;
 
+    int temp;
+
     cout << " Enter the temperature" << endl;
-    cin >> temperature;
+    cin >> temp;
 
-     
+     if(temp > 0 &&  temp < 30){
+        std::cout << " The temperature is good";
+     }else {
+        std::cout << " The temperature is bad";
+     }
 
-    
-
+// || check if at least one of the two conditions is true
+// ! reverses  the logical state of its operand
     return 0;
-}
+} 
